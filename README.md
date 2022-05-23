d685e5eeabced2c35a46e8e9b71fb748fa4e5120
evergardener <2182164075@qq.com>
2022-05-23 23:45:59 +0800

    First backup

---
+++ README.md
@@ -0,0 +1,22 @@
+Packages I installed:
+Terminal:alacritty(but I want to change it)
+Text Editor:Atom
+Clipboard:copyq
+Input Method:fcitx5
+Screenshot:flameshot
+WM:i3
+Status:polybar
+IM:telegram icalingua
+IDE:IDEA CLion
+Music:listen1
+Player:mpv smplayer
+Office:LibreOffice
+Background:feh nitrogen
+Editor:vim -> nvim(nvim-qt)/neovide
+Filemanager:ranger pcmanfm(thunar)
+Launcher:dmenu -> rofi
+Proxy:v2raya
+AUR helper:paru/yay
+Powershell:zsh(oh-my-zsh-powershell10k)
+Mail:Thunerbird
+
---
+++ rofi/powermenu.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Set dir path
+dir="$HOME/.config/rofi"
+
+# Get uptime
+uptime=$(uptime -p | sed -e 's/up //g')
+
+# Options
+shutdown=""
+lock=""
+logout="﫼"
+
+# set powermenu theme
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/powermenu.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$shutdown\n$lock\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p " Uptime: $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			systemctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			bash $HOME/.config/scripts/lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			fi
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
